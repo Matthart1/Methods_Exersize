3ffa5cddc4fa81c06fc63c73eec5742d1dd6d595
Matt Hart <mp.hart047@gamil.com>
2021-02-19 18:34:57 -0600

    MathClass version 2.0

--- Methods_MathClass/Methods_MathClass/Program.cs
+++ Methods_MathClass/Methods_MathClass/Program.cs
@@ -7,12 +7,18 @@ namespace Methods_MathClass
       
         static void Main(string[] args)
         {
+            Console.WriteLine("Sum of 10 plus 20 is...");
+            Console.ReadLine();
             var result = Sum(10, 20);
             Console.ReadLine();
 
+            Console.WriteLine("The result of 5 times 10 is...");
+            Console.ReadLine();
             var math = Multiple(5, 10);
             Console.ReadLine();
-        
+
+            
+
 
         }   
         public static int Sum(int x, int y)
@@ -23,10 +29,13 @@ namespace Methods_MathClass
         }
         public static int Multiple(int x, int y)
         {
+            
             var answer = x * y;
             Console.WriteLine(answer);
+            Console.ReadLine();
+            Console.WriteLine("Great Job!!");
             return answer;
         }
-
+        
     } 
 }
